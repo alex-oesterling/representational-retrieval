b5b04e24cf43dd5545c966bebdfc6e5050f47cc7
Alex Oesterling <alex.oesterling@gmail.com>
2024-04-09 14:41:55 -0400

    cleaned plots

--- experiments/random_sampling.py
+++ experiments/random_sampling.py
@@ -44,43 +44,49 @@ def main():
     functions = generate_tree(depth, attributes)
 
     ## compute aggregate statistics
-    sums = torch.zeros(len(functions))
+    expected_function_values = torch.zeros(len(functions))
     total = 0
     for _, labels in tqdm(dataloader):
         for index, function in enumerate(functions):
-            sums[index] = torch.sum(labels[:, list(function)])
+            expected_function_values[index] = torch.sum(labels[:, list(function)])
         total += labels.shape[0]
-    sums /= total
+    expected_function_values /= total
 
     residuals = []
     ks = []
     worst_functions=[]
-    for k in range(1, 100):
+    for k in tqdm(range(1, 10000, 100)):
         runs = []
         worst_function = []
         for _ in range(5):
-            random_indices = torch.randperm(len(dataset))[:k].tolist()
-            subset = torch.utils.data.Subset(dataset, random_indices)
-            subsetloader = torch.utils.data.DataLoader(subset, batch_size=k)
+            # random_indices = torch.randperm(len(dataset))[:k].tolist()
+            # random_indices = torch.random.randint(high=len(dataset), size=(k)).tolist()
+            sampler =  torch.utils.data.RandomSampler(dataset, replacement=True, num_samples=k)
+            # subset = torch.utils.data.Subset(dataset, random_indices)
+            subsetloader = torch.utils.data.DataLoader(dataset, sampler = sampler, batch_size=k)
             _, random_labels = next(iter(subsetloader))
 
-            retrieved_sums = torch.zeros(len(functions))
+            retrieved_function_value = torch.zeros(len(functions))
             for index, function in enumerate(functions):
-                retrieved_sums[index] = torch.sum(random_labels[:, list(function)])
-            retrieved_sums /= k
+                retrieved_function_value[index] = torch.linalg.norm(random_labels[:, list(function)].float())
+            retrieved_function_value /= k
 
-            runs.append(torch.max(torch.abs(retrieved_sums-sums)).item())
-            worst_function.append(torch.argmax(torch.abs(retrieved_sums-sums)).item())
+            runs.append(torch.max(torch.abs(retrieved_function_value-expected_function_values)).item())
+            worst_function.append(torch.argmax(torch.abs(retrieved_function_value-expected_function_values)).item())
         worst_functions.extend(worst_function)
         residuals.extend(runs)
         ks.extend([k]*5)
-    print("Worst function: ", functions[max(set(worst_functions), key=worst_functions.count)])
+
+    worst_function_indices = functions[max(set(worst_functions), key=worst_functions.count)]
+    print("Worst function: ", worst_function_indices)
+    print("Worst attributes: ", [attributes[i] for i in worst_function_indices])
     
     plotdf = pd.DataFrame({"k": ks, "residual": residuals})
 
     sns.lineplot(data=plotdf, x="k", y="residual")
     plt.xlabel("Retrieval size, k")
-    plt.ylabel(r"Worst case residual (\sup_{c \in \mathcal{C}} |\sum_{i=0}^k c(x_i) - E_{x \sim P_x} c(x))")
+    plt.ylabel("Norm Difference")
+    plt.title("Worst case norm difference for tree of depth " + str(depth))
     plt.savefig("./results/residuals_depth_{}.png".format(depth))
 
 if __name__ == "__main__":
