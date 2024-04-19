3cec1d1eaad554b993830a920187efb7fde67860
Carol Xuan Long <carol_long@g.harvard.edu>
2024-04-19 14:57:17 -0400

    Experimental Results and Sync MMR Experiment

--- experiments/script_MMR.sh
+++ experiments/script_MMR.sh
@@ -8,4 +8,4 @@
 
 module load python/3.10.12-fasrc01
 mamba activate torchy
-python MMR_retrieval.py
+python MMR_retrieval.py -device "cuda" -dataset "celeba" -n_samples 20000 -query "A photo of a Professor" -k 30 -functionclass "linearregression"
